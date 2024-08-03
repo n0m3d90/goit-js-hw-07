349722d8e99672623302a499461ea972def0fc8e
n0m3d90 <kivu_flrn90@yahoo.com>
2024-08-03 13:40:59 +0300

    up tema doi

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const createGalleryMarkup = (items) => {
+    return items.map(({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `).join('');
+  };
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const galleryContainer = document.querySelector('.gallery');
+    galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+  
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captions: true,
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+  });
